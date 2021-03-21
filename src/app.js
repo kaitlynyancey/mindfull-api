0a845f6542281110149734ea307202b7b81fd2f0
Kaitlyn Yancey <kyancey423@gmail.com>
2021-03-21 15:39:03 -0400

    adding authorization

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { NODE_ENV } = require('./config')
 const {CLIENT_ORIGIN} = require('./config');
 const usersRouter = require('./users/users-router')
 const entriesRouter = require('./entries/entries-router')
+const logger = require('./logger')
 
 const app = express()
 
@@ -21,6 +22,17 @@ app.use(
     cors({
         origin: CLIENT_ORIGIN
 }))
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`)
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
 
 app.use('/api/users', usersRouter)
 
