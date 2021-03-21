c1c1a0b775dffc97cbef49c668d74fcea2f145fa
Kaitlyn Yancey <kyancey423@gmail.com>
2021-03-21 11:17:21 -0400

    updating for initial deployment

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const {CLIENT_ORIGIN} = require('./config');
 
 const app = express()
+
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
@@ -13,7 +15,10 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
